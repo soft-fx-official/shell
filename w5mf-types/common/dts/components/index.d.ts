c49e5e336446fdb83e81691fdfa4de21347f838a
Ilya Fedotov <ilia.fedotov@softnet.by>
2022-11-24 12:48:07 +0400

    removed all unnecessary and moved to the common

--- w5mf-types/common/dts/components/index.d.ts
+++ w5mf-types/common/dts/components/index.d.ts
@@ -2,5 +2,3 @@ export type { IErrorBoundary } from './ErrorBoundary';
 export { ErrorBoundary } from './ErrorBoundary';
 export type { IModuleLoader } from './ModuleLoader';
 export { ModuleLoader } from './ModuleLoader';
-export type { RedirectProps } from './Redirect';
-export { Redirect } from './Redirect';
