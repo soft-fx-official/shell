6ce6275c19b19a720d50accc9a8e9bb4e895511e
Ilya Fedotov <ilia.fedotov@softnet.by>
2022-11-28 13:41:55 +0400

    update types

--- w5mf-types/common/dts/inits/app.d.ts
+++ w5mf-types/common/dts/inits/app.d.ts
@@ -1,26 +1,26 @@
-import { i18n } from 'i18next'
-import { IStep } from '../models'
-import { IBus } from '../tools'
-import { IInitStateR } from './state'
-import { IInitStorageR } from './storage'
+import { i18n } from 'i18next';
+import { IStep } from '../models';
+import { IBus } from '../tools';
+import { IInitStateR } from './state';
+import { IInitStorageR } from './storage';
 interface Iconfig {
-  appName: string
-  theme: string
-  steps?: IStep[]
-  demoAccountStepId?: string
-  startStepId?: string
-  [key: string]: any
+    appName: string;
+    theme: string;
+    steps?: IStep[];
+    demoAccountStepId?: string;
+    startStepId?: string;
+    [key: string]: any;
 }
 interface IInitApp {
-  storage: IInitStorageR
-  i18next: i18n
-  state: IInitStateR
-  bus: IBus
-  api: any
-  isRootApp: boolean
-  config: Iconfig
+    storage: IInitStorageR;
+    i18next: i18n;
+    state: IInitStateR;
+    bus: IBus;
+    api: any;
+    isRootApp: boolean;
+    config: Iconfig;
 }
-declare type TinitApp = (args: IInitApp) => Promise<void>
-declare function init(args: IInitApp, initCurrentApp: TinitApp): Promise<void>
-export { init as initApp }
-export type { Iconfig, IInitApp, TinitApp }
+declare type TinitApp = (args: IInitApp) => Promise<void>;
+declare function init(args: IInitApp, initCurrentApp: TinitApp): Promise<void>;
+export { init as initApp };
+export type { Iconfig, IInitApp, TinitApp };
